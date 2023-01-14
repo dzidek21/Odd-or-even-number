6658cb4eab4808bfb5f9df7893325ffb12afd5f6
kamil witkowski <kamilwitkowski5@gmail.com>
2023-01-14 13:47:33 +0100

    V 0.1

--- Odd or even number/Program.cs
+++ Odd or even number/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Cześć!\nPodaj jakąś liczbę:");
+            var numberToCheck=UserNumber();
+            Console.WriteLine($"Liczba {numberToCheck} jest liczbą {CheckNumber(numberToCheck)}");
+            Console.ReadLine();       
+        }
+        private static int UserNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                {
+                    Console.WriteLine("Podano zły znak.\nPodaj liczbę");
+                    continue;
+                }
+                else
+                {         
+                    return userNumber;
+                    break;
+                }          
+            }
+        }
+        private static string CheckNumber(int numberToCheck)
+        {
+            if (numberToCheck%2==0)
+            {
+                return "parzystą";
+            }
+            else
+            {
+                return "nieparzystą";
+            }
         }
     }
 }
